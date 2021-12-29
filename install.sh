499fd5c7e714adde881637fc90c7ee8311c555a2
DN-debug <75267055+DN-debug@users.noreply.github.com>
2021-12-30 03:03:32 +0530

    adding install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# =================================================================== #
+# Author : DN-debug
+# Description : Simple installation script for GDM-Background-Changer.
+# =================================================================== #
+
+cp -r frontend-gbc gdm-background-changer /usr/local/bin/
+cp -r gdm-background-changer.desktop /usr/share/applications/
